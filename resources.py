32d81689fd76b237304c603980cfd386aca6f1b1
feitzin <fl2301@columbia.edu>
2023-02-21 20:32:46 -0500

    moved human judgements file out of nested summeval repo

---
+++ resources.py
@@ -0,0 +1,6 @@
+webnlg_dir = '/Users/feitzin/research/amazon-internship/challenge-2020/evaluation/human-evaluation/results/en'
+summeval_dir = '/Users/feitzin/research/amazon-internship/SummEval'
+summeval_path = 'summeval_human_judgements.jsonl'
+
+temp_output_f = 'tmp/output.amr'
+temp_target_f = 'tmp/target.amr'
