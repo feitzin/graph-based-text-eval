f8b28f445220dc263dc7488bfc22f57dec569333
Fei-Tzin Lee <fl2301@columbia.edu>
2023-02-21 20:34:08 -0500

    default resources

--- resources.py
+++ resources.py
@@ -1,5 +1,5 @@
-webnlg_dir = '/Users/feitzin/research/amazon-internship/challenge-2020/evaluation/human-evaluation/results/en'
-summeval_dir = '/Users/feitzin/research/amazon-internship/SummEval'
+webnlg_dir = 'challenge-2020/evaluation/human-evaluation/results/en'
+summeval_dir = 'SummEval'
 summeval_path = 'summeval_human_judgements.jsonl'
 
 temp_output_f = 'tmp/output.amr'
