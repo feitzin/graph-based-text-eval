22d7d60c6906825f9ff24e0650a56c076a43ba65
Fei-Tzin Lee <fl2301@columbia.edu>
2023-03-23 03:58:46 -0400

    current version of system-level metric computation. unclear if working properly

--- data.py
+++ data.py
@@ -3,10 +3,16 @@ import json
 import spacy
 import numpy as np
 
+import sys
+sys.path.insert(0, 'transition-amr-parser')
+from transition_amr_parser.parse import AMRParser
+
 from resources import webnlg_dir, summeval_dir, summeval_path
 
 def get_handle(n, handles):
     prefix = n.text[0]
+    if not prefix.isalpha():
+        prefix = 'x'
     if prefix not in handles:
         handles[prefix] = n
         return prefix, handles
@@ -20,7 +26,8 @@ def get_handle(n, handles):
 
 def linearize_single(n, handles):
     handle, handles = get_handle(n, handles)
-    linearized = f'( {handle} / {n.text} '
+    token = n.text if n.text.isalpha() else f'"{n.text}"'
+    linearized = f'( {handle} / {token} '
     for c in n.children:
         child, handles = linearize_single(c, handles)
         linearized += f'{c.dep_}: {child} '
@@ -45,24 +52,48 @@ def linearize_dependency(sents, nlp=None, clip=True):
 
     return linearized
 
+def parse_amr(sents, parser=None,
+                  clean=True,
+                  checkpoint='DATA/AMR3.0/models/amr3.0-structured-bart-large-neur-al/seed42/checkpoint_wiki.smatch_top5-avg.pt'):
+    if parser is None:
+        parser = AMRParser.from_checkpoint(checkpoint)#, None)
+    if clean:
+        sents = [''.join(filter(lambda x: x in printable, s)) for s in sents]
+    sent_tokens = [parser.tokenize(s)[0] for s in sents]
+    amrs = []
+    for s in sent_tokens:
+        try:
+            annotations, decoding_data = parser.parse_sentence(s)
+            amrs.append(decoding_data.get_amr())
+        except Exception as e:
+            print(f'Error: {e} on sentence {s}; skipping')
+            amrs.append(None)
+    linearized = [amr.to_penman(jamr=False, isi=False) if amr is not None else None for amr in amrs]
+    assert len(linearized) == len(sents)
+    return linearized
+
 def read_webnlg():
     pass
 
 def read_summeval(f=summeval_path):
     full = [json.loads(l) for l in open(f, 'r').readlines()]
-    targets = []
-    outputs = []
+    targets = {}
+    outputs = {}
     scores = {}
     metrics = ['coherence', 'consistency', 'fluency', 'relevance']
 
-    for m in metrics:
-        scores[m] = []
-    
     for d in full:
-        outputs.append(d['decoded'])
-        targets.append(d['references'])
+        mid = d['model_id']
+        if mid not in outputs:
+            outputs[mid] = []
+            targets[mid] = []
+            scores[mid] = {}
+        outputs[mid].append(d['decoded'])
+        targets[mid].append(d['references'])
         for m in metrics:
-            scores[m].append([np.mean(a[m]) for a in d['expert_annotations']])
+            if m not in scores[mid]:
+                scores[mid][m] = []
+            scores[mid][m].append(np.mean([a[m] for a in d['expert_annotations']]))
     return outputs, targets, scores
 
 def read_data(dataset):
--- metrics.py
+++ metrics.py
@@ -3,49 +3,103 @@ import json
 import subprocess
 import argparse
 
+import numpy as np
+from scipy.stats import kendalltau
 import spacy
 import smatch
 from torchmetrics.text.rouge import ROUGEScore
+import corenlp
 
-from data import read_data, linearize_dependency, exists, save, load
+import sys
+sys.path.insert(0, 'SummEval/evaluation')
+from summ_eval.rouge_metric import RougeMetric
+sys.path.insert(0, 'transition-amr-parser')
+from transition_amr_parser.parse import AMRParser
+
+from data import read_data, linearize_dependency, parse_amr, exists, save, load
 from resources import temp_output_f, temp_target_f
 
 def score_sembleu(candidates, targets):
+    print('Scoring sembleu...')
     scores = []
+    skipped = 0
     for c, t in zip(candidates, targets):
+        if c is None or t is None:
+            scores.append(None)
+            skipped += 1
+            continue
         with open(temp_output_f, 'w') as outf, open(temp_target_f, 'w') as targf:
             outf.write(c)
             targf.write(t)
-        output = subprocess.run(['smatch.py', '--ms', '-f', temp_output_f, temp_target_f], capture_output=True).stdout
-        scores.append(float(output.split('\n')[-2]))
+        output = subprocess.run(['./sembleu/eval.sh', temp_output_f, temp_target_f],
+                                    capture_output=True, encoding='utf-8').stdout
+        output = output.strip().split('\n')
+        try:
+            assert len(output) == 4
+        except:
+            scores.append(None)
+            skipped += 1
+            continue
+        output = output[2]
+        scores.append(float(output))
+    assert len(scores) == len(candidates)
+    print(f'...scored {len(scores)} pairs; skipped {skipped}.')
     return scores
 
 def score_smatch(candidates, targets):
+    print('Scoring smatch...')
+    blank = []
     with open(temp_output_f, 'w') as outf, open(temp_target_f, 'w') as targf:
+        i = -1
         for c, t in zip(candidates, targets):
-            outf.write(c + '\n')
-            targf.write(t + '\n')
-    args = ['smatch.py', '--ms', '-f', temp_output_f, temp_target_f]
+            i += 1
+            if c is None or t is None:
+                outf.write('(p / placeholder)\n\n')
+                targf.write('(p / placeholder)\n\n')
+                blank.append(i)
+                continue
+            outf.write(c.strip() + '\n\n')
+            targf.write(t.strip() + '\n\n')
+    args = ['smatch.py', '--ms', '--significant', '4', '-f', temp_output_f, temp_target_f]
     output = subprocess.run(args, text=True, capture_output=True)
-    scores = [x.strip() for x in output.stdout.split('\n')]
+    scores = [x.strip() for x in output.stdout.strip().split('\n')]
     scores = [float(x.split('F-score: ')[-1]) for x in scores]
+    scores = [s if i not in blank else None for i, s in enumerate(scores)]
+    assert len(scores) == len(candidates)
+    print(f'...scored {len(scores)} pairs; skipped {len(blank)}.')
     return scores
 
 def score(outputs, targets):
     print('Scoring AMR metrics...')
-    smatch = score_smatch(outputs, targets)
-    sembleu = score_sembleu(outputs, targets)
+    smatch = {'smatch': score_smatch(outputs, targets)}
+    sembleu = {'sembleu': score_sembleu(outputs, targets)}
 
     return smatch, sembleu
 
-def score_rouge(outputs, targets):
+def score_rouge(outputs, targets, implementation='textmetrics', pretokenize=False):
     '''
     Collects only ROUGE scores.
     '''
-    print('Scoring on ROUGE...')
+    if implementation == 'summeval':
+        rouge = RougeMetric()
+        if pretokenize:
+            with corenlp.CoreNLPClient(annotators='tokenize') as client:
+                targets = [' '.join([w.word for w in client.annotate(t).sentencelessToken]) for t in targets]
+        #scores = rouge.evaluate_batch(outputs, [[t] for t in targets])['rouge']
+        scores = {}
+        for o, t in zip(outputs, targets):
+            single = rouge.evaluate_example(o, t)['rouge']
+            for m in single:
+                if m not in scores:
+                    scores[m] = []
+                scores[m].append(single[m])
+        scores = {m: [scores[m]] for m in scores}
+        return scores
+    
     rouge = ROUGEScore()
     scores = {}
     i = 1
+    
     for o, t in zip(outputs, targets):
         single = rouge(o, t)
         for k in single:
@@ -62,17 +116,94 @@ def print_banner(s):
     print(s)
     print('--------------------------------')
 
+def print_table(t):
+    cols = [str(k) for k in t.keys()]
+    rows = [str(k) for k in t[cols[0]].keys()]
+
+    print(f'{"(metric/aspect)":20}' + '\t  ' + '  '.join(cols))
+    for r in rows:
+        print(f'{r:24}' + '\t' + '\t'.join([f'{t[c][r].statistic:0.3f}' for c in cols]))
+
+    print()
+
 def print_correlations(first, second):
     '''
     Compute and print correlations between two dictionaries of scores.
     '''
-    for metric in first:
-        for second_metric in second:
-            corr = np.corrcoef(first[metric], second[second_metric])
-            print(f'Correlation between {metric} and {second_metric}: {corr}')
-        print()
+    best = {}
+    fbest = {}
 
-def score_all(dataset, output_base, multi_references=False, overwrite=False):
+    correlations = {}
+    
+    for metric in first:
+        best[metric] = None
+        fbest[metric] = None
+        correlations[metric] = {}
+        for second_metric in second:
+            if second_metric.endswith('_cb') or second_metric.endswith('_ce'):
+                continue
+            #corr = np.corrcoef(first[metric], second[second_metric])[0][-1]
+            try:
+                assert len(first[metric]) == len(second[second_metric])
+            except:
+                print(f'Failed on length check: metric {metric} has {len(first[metric])} elements and metric {second_metric} has {len(second[second_metric])} elements.')
+                exit(0)
+            l = len(first[metric])
+            filtered_first = [first[metric][i] for i in range(l) if first[metric][i] is not None and second[second_metric][i] is not None]
+            filtered_second = [second[second_metric][i] for i in range(l) if first[metric][i] is not None and second[second_metric][i] is not None]
+            assert len(filtered_first) == len(filtered_second)
+            corr = kendalltau(filtered_first, filtered_second)
+            if best[metric] == None or corr > best[metric][1]:
+                best[metric] = (second_metric, corr)
+            #if second_metric.endswith('fmeasure') and (fbest[metric] == None or corr > fbest[metric][1]):
+            if second_metric.endswith('f_score') and (fbest[metric] == None or corr > fbest[metric][1]):
+                fbest[metric] = (second_metric, corr)
+            correlations[metric][second_metric] = corr
+            #print(f'Correlation between {metric} and {second_metric}: {corr}')
+        #print()
+
+    print_table(correlations)
+
+    #print(best)
+    #print()
+    #print(fbest)
+
+def aggregate(scores):
+    '''
+    Aggregates a dict of metric scores into system-level and individual-level scores.
+    '''
+    keys = [x for x in scores.keys()]
+    keys.sort()
+    if type(scores[keys[0]]) == dict:
+        metrics = [k for k in scores[keys[0]].keys()]
+        metrics.sort()
+        model_level = {metric: [np.mean([x for x in scores[m][metric] if x is not None]) for m in keys] for metric in metrics}
+        total = {}
+        for metric in metrics:
+            if metric not in total:
+                total[metric] = []
+            for model in keys:
+                if len(scores[model][metric]) == 1:
+                    if type(scores[model][metric]) == list:
+                        total[metric] += scores[model][metric][0]
+                    else:
+                        assert type(scores[model][metric]) == dict
+                        key = [k for k in scores[model][metric].keys()][0]
+                        total[metric] += scores[model][metric][key]
+                else:
+                    total[metric] += scores[model][metric]
+        return model_level, total
+    
+    model_level = {m: np.mean(scores[m]) for m in scores}
+    total = []
+    for m in scores:
+        total += scores[m]
+
+    return model_level, total
+
+def score_all(dataset, output_base,
+                  parser_checkpoint='DATA/AMR3.0/models/amr3.0-structured-bart-large-neur-al/seed42/checkpoint_wiki.smatch_top5-avg.pt',
+                  multi_references=True, overwrite=False):
     '''
     Scoring pipeline.
     '''
@@ -84,7 +215,7 @@ def score_all(dataset, output_base, multi_references=False, overwrite=False):
     if overwrite or not exists(output_dir, fields):
         outputs, targets, scores = read_data(args.dataset)
         if not multi_references:
-            targets = [t[0] for t in targets]
+            targets = {m: [t[0] for t in targets[m]] for m in targets}
         save([outputs, targets, scores], output_dir, fields)
     else:
         outputs, targets, scores = load(output_dir, fields)
@@ -94,44 +225,110 @@ def score_all(dataset, output_base, multi_references=False, overwrite=False):
     if overwrite or not exists(output_dir, fields):
         print('Linearizing dependencies...')
         nlp = spacy.load('en_core_web_trf')
-        outputs_dep = [linearize_dependency(s, nlp) for s in outputs]
-        targets_dep = [linearize_dependency(s, nlp) for s in targets]
+        outputs_dep = {m: linearize_dependency(outputs[m], nlp) for m in outputs}
+        targets_dep = {m: linearize_dependency(targets[m], nlp) for m in targets}
         save([outputs_dep, targets_dep], output_dir, fields)
     else:
         outputs_dep, targets_dep = load(output_dir, fields)
 
+    # get amr
+    fields = ['outputs_amr', 'targets_amr']
+    if overwrite or not exists(output_dir, fields):
+        print('Parsing AMR...')
+        parser = None
+        if parser_checkpoint:
+            parser = AMRParser.from_checkpoint(parser_checkpoint)
+        outputs_amr = {m: parse_amr(outputs[m], parser) for m in outputs}
+        targets_amr = {m: parse_amr(targets[m], parser) for m in targets}
+        save([outputs_amr, targets_amr], output_dir, fields)
+    else:
+        outputs_amr, targets_amr = load(output_dir, fields)
+        
     # calculate rouge scores
     fields = ['rouge_scores']
     if overwrite or not exists(output_dir, fields):
-        rouge_scores = score_rouge(outputs, targets)
+        print('Scoring ROUGE...')
+        rouge_scores = {m: score_rouge(outputs[m], targets[m]) for m in outputs}
         save([rouge_scores], output_dir, fields)
     else:
-        rouge_scores = load(output_dir, fields)
+        rouge_scores = load(output_dir, fields)[0]
 
     # calculate amr scores
-    fields = ['smatch_scores', 'sembleu_scores']
+    fields = ['smatch_dep', 'sembleu_dep']
     if overwrite or not exists(output_dir, fields):
-        smatch_scores, sembleu_scores = score(outputs_dep, targets_dep)
-        save([smatch_scores, sembleu_scores], output_dir, fields)
+        smatch_dep = {}
+        sembleu_dep = {}
+        for m in outputs_dep:
+            smatch_dep[m], sembleu_dep[m] = score(outputs_dep[m], targets_dep[m])
+        save([smatch_dep, sembleu_dep], output_dir, fields)
     else:
-        smatch_scores, sembleu_scores = load(output_dir, fields)
+        smatch_dep, sembleu_dep = load(output_dir, fields)
+
+    # calculate amr scores
+    fields = ['smatch_amr', 'sembleu_amr']
+    if overwrite or not exists(output_dir, fields):
+        smatch_amr = {}
+        sembleu_amr = {}
+        for m in outputs_amr:
+            smatch_amr[m], sembleu_amr[m] = score(outputs_amr[m], targets_amr[m])
+        save([smatch_amr, sembleu_amr], output_dir, fields)
+    else:
+        smatch_amr, sembleu_amr = load(output_dir, fields)
+
+    rouge_model, rouge_all = aggregate(rouge_scores)
+    original_model, original_all = aggregate(scores)
+
+    keys = [k for k in smatch_amr.keys()]
+
+    smatch_dep_model, smatch_dep_all = aggregate(smatch_dep)
+    smatch_amr_model, smatch_amr_all = aggregate(smatch_amr)
+    sembleu_dep_model, sembleu_dep_all = aggregate(sembleu_dep)
+    sembleu_amr_model, sembleu_amr_all = aggregate(sembleu_amr)
 
     # print
-    print_banner('ROUGE correlations')
-    print_correlations(scores, rouge_scores)
+    print_banner('ROUGE system level')
+    print_correlations(original_model, rouge_model)
 
-    print_banner('Smatch correlations')
-    print_correlations(scores, smatch_scores)
+    print_banner('Smatch dependency system level')
+    print_correlations(original_model, smatch_dep_model)
 
-    print_banner('Sembleu correlations')
-    print_correlations(scores, sembleu_scores)
+    print_banner('Smatch AMR system level')
+    print_correlations(original_model, smatch_amr_model)
+
+    print_banner('Sembleu dependency system level')
+    print_correlations(original_model, sembleu_dep_model)
+
+    print_banner('Sembleu AMR system level')
+    print_correlations(original_model, sembleu_amr_model)
+
+    print_banner('ROUGE individual')
+    print_correlations(original_all, rouge_all)
+
+    print_banner('Smatch dependency individual')
+    print_correlations(original_all, smatch_dep_all)
+
+    print_banner('Smatch AMR individual')
+    print_correlations(original_all, smatch_amr_all)
+
+    print_banner('Sembleu dependency individual')
+    print_correlations(original_all, sembleu_dep_all)
+
+    print_banner('Sembleu AMR individual')
+    print_correlations(original_all, sembleu_amr_all)
 
     print_banner('Correlations between ROUGE and AMR metrics')
-    print_correlations(rouge_scores, smatch_scores)
-    print_correlations(rouge_scores, sembleu_scores)
+    print_correlations(smatch_dep_all, rouge_all)
+    print_correlations(sembleu_dep_all, rouge_all)
+    print_correlations(smatch_amr_all, rouge_all)
+    print_correlations(sembleu_amr_all, rouge_all)
 
     print_banner('Correlations between AMR metrics')
-    print_correlations(smatch_scores, sembleu_scores)
+    print_correlations(smatch_dep_all, sembleu_dep_all)
+    print_correlations(smatch_amr_all, sembleu_dep_all)
+    print_correlations(smatch_dep_all, sembleu_amr_all)
+    print_correlations(smatch_amr_all, sembleu_amr_all)
+    print_correlations(smatch_dep_all, smatch_amr_all)
+    print_correlations(sembleu_dep_all, sembleu_amr_all)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
